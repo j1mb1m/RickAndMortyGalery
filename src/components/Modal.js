02d810b1b3741b66c65a289c528d030203037939
Dasha Astakhava <dasha.kozhalo@gmail.com>
2023-08-01 21:39:17 +0300

    changed styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,20 +6,19 @@ import './modal.scss';
 const Modal = ({ active, setActive, item }) => {
     return <div className={active ? "Modal active" : "Modal"} onClick={() => setActive(false)}>
         <div className="Modal__content"
-            style={{
-                backgroundImage: `linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,1) 65%) , url(${item.image})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'auto 100%',
-            }}
             onClick={e => e.stopPropagation()}>
-            <div className="Modal__description">
-                <div><span>Name: </span>{item.name}</div>
-                <div><span>Origin: </span>{item.origin}</div>
-                <div><span>Status: </span>{item.status}</div>
-                <div><span>Location: </span>{item.location}</div>
-                <div><span>Species: </span>{item.species}</div>
-                <div><span>Gender: </span>{item.gender}</div>
+            <img src={item.image} alt={item.name} />
+            <div className="Modal__container">
+                <div className="Modal__description">
+                    <div><span>Name: </span>{item.name}</div>
+                    <div><span>Origin: </span>{item.origin}</div>
+                    <div><span>Status: </span>{item.status}</div>
+                    <div><span>Location: </span>{item.location}</div>
+                    <div><span>Species: </span>{item.species}</div>
+                    <div><span>Gender: </span>{item.gender}</div>
+                </div>
             </div>
+
         </div>
     </div >
 }
